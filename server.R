c6b2b2849bd9d7ccb82b089e794da1cb465f54d2
Kelly Ho <kellyho15@gmail.com>
2018-07-24 23:26:32 -0400

    file updated

--- server.R
+++ server.R
@@ -24,25 +24,25 @@ shinyServer(function(input, output){
     # youth state data over 2007, 2009, 2011, 2013, 2015
     output$Y_trend_fru <- renderPlot(
       ggplot(y_spread, aes(x=`Fruit consumption`, y=Obesity)) + geom_point() + geom_smooth(method = 'lm') +   # obesity vs fruit
-        labs(x= "Fruit consumption population (%)", y="Obesity population (%)") +
+        labs(x= "Fruit consumption population (%)", y="Obese population (%)") +
         theme(axis.title = element_text(size=13),
               axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
     )
     output$Y_trend_veg <- renderPlot(
       ggplot(y_spread, aes(x=`Vegetables consumption`, y=Obesity)) + geom_point() + geom_smooth(method = 'lm') +   # obesity vs veggie
-        labs(x= "Vegetables consumption population (%)", y="Obesity population (%)") +
+        labs(x= "Vegetables consumption population (%)", y="Obese population (%)") +
         theme(axis.title = element_text(size=13),
               axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
     )
     output$Y_trend_pa1 <- renderPlot(
       ggplot(y_spread, aes(x=`>1 hr daily physical activity`, y=Obesity)) + geom_point() + geom_smooth(method = 'lm') +  # obesity vs PA
-        labs(x= ">1 hr daily physical activity population (%)", y="Obesity population (%)") +
+        labs(x= ">1 hr daily physical activity population (%)", y="Obese population (%)") +
         theme(axis.title = element_text(size=13),
               axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
     )
     output$Y_trend_pa2 <- renderPlot(
       ggplot(y_spread, aes(x=`Daily PE class`, y=Obesity)) + geom_point() + geom_smooth(method = 'lm') +  # obesity vs PA
-        labs(x= "Daily PE class population (%)", y="Obesity population (%)") +
+        labs(x= "Daily PE class population (%)", y="Obese population (%)") +
         theme(axis.title = element_text(size=13),
               axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
     )
@@ -51,43 +51,43 @@ shinyServer(function(input, output){
     # adult state data over 2011, 2013, 2015
     output$A_trend_fru <- renderPlot(
       ggplot(a_spread, aes(x=`Fruit consumption`, y=Obesity)) + geom_point() + geom_smooth(method = 'lm') +  # obesity vs fruit
-        labs(x= "Fruit consumption population (%)", y="Obesity population (%)") +
+        labs(x= "Fruit consumption population (%)", y="Obese population (%)") +
         theme(axis.title = element_text(size=13),
               axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
     )
     output$A_trend_veg <- renderPlot(
       ggplot(a_spread, aes(x=`Vegetables consumption`, y=Obesity)) + geom_point() + geom_smooth(method = 'lm') +  # obesity vs veggie
-        labs(x= "Vegetables consumption population (%)", y="Obesity population (%)") +
+        labs(x= "Vegetables consumption population (%)", y="Obese population (%)") +
         theme(axis.title = element_text(size=13),
               axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
     )
     output$A_trend_pa1 <- renderPlot(
       ggplot(a_spread, aes(x=`Short duration aerobic`, y=Obesity)) + geom_point() + geom_smooth(method = 'lm') +   # obesity vs PA
-        labs(x= "Short duration aerobic population (%)", y="Obesity population (%)") +
+        labs(x= "Short duration aerobic population (%)", y="Obese population (%)") +
         theme(axis.title = element_text(size=13),
               axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
     )
     output$A_trend_pa2 <- renderPlot(
       ggplot(a_spread, aes(x=`Short duration aerobic and strengthening`, y=Obesity)) + geom_point() + geom_smooth(method = 'lm') +  # obesity vs PA
-        labs(x= "Short duration aerobic and strengthening population (%)", y="Obesity population (%)") +
+        labs(x= "Short duration aerobic and strengthening population (%)", y="Obese population (%)") +
         theme(axis.title = element_text(size=13),
               axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
     )
     output$A_trend_pa3 <- renderPlot(
       ggplot(a_spread, aes(x=`Long duration aerobic`, y=Obesity)) + geom_point() + geom_smooth(method = 'lm') +  # obesity vs PA
-        labs(x= "Long duration aerobic population (%)", y="Obesity population (%)") +
+        labs(x= "Long duration aerobic population (%)", y="Obese population (%)") +
         theme(axis.title = element_text(size=13),
               axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
     )
     output$A_trend_pa4 <- renderPlot(
       ggplot(a_spread, aes(x=`Strengthening`, y=Obesity)) + geom_point() + geom_smooth(method = 'lm') +  # obesity vs PA
-        labs(x= "Strengthening population (%)", y="Obesity population (%)") +
+        labs(x= "Strengthening population (%)", y="Obese population (%)") +
         theme(axis.title = element_text(size=13),
               axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
     )
     output$A_trend_pa5 <- renderPlot(
       ggplot(a_spread, aes(x=`No physical activity`, y=Obesity)) + geom_point() + geom_smooth(method = 'lm') +   # obesity vs PA
-        labs(x= "No physical activity population (%)", y="Obesity population (%)") +
+        labs(x= "No physical activity population (%)", y="Obese population (%)") +
         theme(axis.title = element_text(size=13),
               axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
     )
@@ -99,7 +99,7 @@ shinyServer(function(input, output){
       ay_combine %>%
         filter(!Stratification == "Total", Question == "Obesity", Data == "youth", 
                LocationDesc == input$Y_Selected_LocationDesc, StratificationCategory  == input$Y_Selected_cat) %>%
-        ggplot(aes(x=Year, y=Data_Value)) + labs(y="Obesity population (%)") + 
+        ggplot(aes(x=Year, y=Data_Value)) + labs(y="Obese population (%)") + 
         scale_y_continuous(limits = c(0, 40)) + scale_x_continuous(breaks = seq(2001, 2015, by = 2)) +
         geom_bar(stat ="identity", position = "dodge", fill="tomato1") + facet_grid(~ Stratification) +
         theme(strip.text = element_text(size=15), axis.title = element_text(size=13),
@@ -110,7 +110,7 @@ shinyServer(function(input, output){
         if (input$A_Selected_cat == "Education"){
         edu %>%
         filter(Question == "Obesity") %>%
-        ggplot(aes(x=Year, y=Data_Value)) + labs(y="Obesity population (%)") + 
+        ggplot(aes(x=Year, y=Data_Value)) + labs(y="Obese population (%)") + 
         scale_y_continuous(limits = c(0, 40)) + scale_x_continuous(breaks = seq(2011, 2016, by = 1)) +
         geom_bar(stat ="identity", position = "dodge", fill="tomato1") + facet_grid(~ Stratification) +
         theme(strip.text = element_text(size=15), axis.title = element_text(size=13),
@@ -119,7 +119,7 @@ shinyServer(function(input, output){
         ay_combine %>%
         filter(!Stratification == "Total", Question == "Obesity", Data == "adult",
                LocationDesc == input$A_Selected_LocationDesc, StratificationCategory == input$A_Selected_cat) %>%
-        ggplot(aes(x=Year, y=Data_Value)) + labs(y="Obesity population (%)") + 
+        ggplot(aes(x=Year, y=Data_Value)) + labs(y="Obese population (%)") + 
         scale_y_continuous(limits = c(0, 40)) + scale_x_continuous(breaks = seq(2011, 2016, by = 1)) +
         geom_bar(stat ="identity", position = "dodge", fill="tomato1") + facet_grid(~ Stratification) +
         theme(strip.text = element_text(size=15), axis.title = element_text(size=13),
@@ -134,7 +134,7 @@ shinyServer(function(input, output){
       ay_combine %>% 
         filter(!Stratification == "Total", !LocationDesc == "National", Year == 2015,
                StratificationCategory == "Age", Question == "Obesity") %>%   #filter obesity, age data
-        ggplot(aes(x=Stratification, y=Data_Value)) + labs(x= "Age", y="Obesity population (%)") + 
+        ggplot(aes(x=Stratification, y=Data_Value)) + labs(x= "Age", y="Obese population (%)") + 
         geom_boxplot() + geom_point(alpha = 0.15) +
         theme(strip.text = element_text(size=15), axis.title = element_text(size=13),
               axis.text.x = element_text(size=14), axis.text.y = element_text(size=14))
